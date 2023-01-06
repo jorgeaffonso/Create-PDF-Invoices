9b22f367dd12f48d7fd62b7ed37e31f7286a1bb1
Jorge <jorgemail.affonso@gmail.com>
2023-01-06 11:44:24 -0300

    day 26 - Add a table.

--- main.py
+++ main.py
@@ -6,11 +6,46 @@ from pathlib import Path
 filepaths = glob.glob("invoice\*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=16)
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split(sep='-')[0]
+    invoice_nr, date = filename.split(sep='-')
+
+    pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=0, h=12, txt=f"Invoice_nr.{invoice_nr}", align="L", ln=1, border=0)
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=0, h=12, txt=f"Date: {date}", align="L", ln=1, border=0)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add Header
+    columns = df.columns
+#            x = 0
+#            for item in columns:
+#                columns[x] = item.replace("_"," ").title()
+#                x= x+1
+    # Replace code above for tha "List Comprehension" code below
+    columns = [item.replace("_"," ").title() for item in columns]
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
     pdf.output(f"PDFs\{filename}.pdf")
